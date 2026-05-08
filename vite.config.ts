352a5c55d8c546cc80ca6fde5c6d457ac27f9191
Lily1756 <1756281616@qq.com>
2026-05-09 01:32:39 +0800

    chore: 更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,13 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
   }
 })
